13ca17762e7593c76593284b8d9f8ff85e17a045
DuskSpiral <52051860+DuskSpiral@users.noreply.github.com>
2022-04-05 00:09:28 -0400

    Added initial structure for server side API routing and virtual database code.
    Added initial structure for server side API routing and virtual database code.

--- client/src/routes/politicians.jsx
+++ client/src/routes/politicians.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getPoliticians } from "../data/politicians";
 
@@ -7,6 +8,37 @@ export default function Politicians() {
   // REPLACE WITH MONGODB CALL
   // get all politicians data object
   let politicians = getPoliticians();
+
+  // React function to handle data requests
+  useEffect(() => {
+    // function to call API and get unique politician list
+    async function getPoliticiansUnique() {
+      // build data slug for API request
+      const dataSlugMongoTest = {
+        requestType: "uniqueCongress",
+        requestData: {
+        }
+      }
+
+      // build options for API request
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataSlugMongoTest)
+      }
+
+      // fetch request to server API
+      console.log("Sending api request...");
+      console.log(dataSlugMongoTest);
+      // const response = await fetch('http://localhost:5000/apiTEST', options);
+      // console.log(response.message);
+    }
+
+    // call async function to get data
+    getPoliticiansUnique();
+  });
 
   // generate a link for every politician on data object
   return (
